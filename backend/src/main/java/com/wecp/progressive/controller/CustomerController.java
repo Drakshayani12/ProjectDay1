2a83377c42106f5b076c7d23d1c46b76093ac0fb
username <wecp-dummy@gmail.com>
2024-01-29 16:13:26 +0000

    day5 changes

--- backend/src/main/java/com/wecp/progressive/controller/CustomerController.java
+++ backend/src/main/java/com/wecp/progressive/controller/CustomerController.java
@@ -3,42 +3,48 @@ package com.wecp.progressive.controller;
 
 import com.wecp.progressive.entity.Customers;
 import com.wecp.progressive.entity.Transactions;
+import com.wecp.progressive.service.CustomerService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.SQLException;
 import java.util.List;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-
 
 @RestController
+@RequestMapping("/customers")
 public class CustomerController {
-    
-@GetMapping("/customer")
-public ResponseEntity<List<Customers>> getAllCustomers() {
-        return null;
-    }
-    @GetMapping("/customer/{customerID}")
-    public ResponseEntity<Customers> getCustomerById(int customerId) {
-        return null;
-    }
-@PostMapping("/customers")
-    public ResponseEntity<Integer> addCustomer(Customers customers) {
-        return null;
-    }
-@PutMapping("customers/{customerID}")
 
-    public ResponseEntity<Void> updateCustomer(int customerId, Customers customers) {
-        return null;
+    @Autowired
+    CustomerService customerService;
+
+    @GetMapping
+    public ResponseEntity<List<Customers>> getAllCustomers() throws SQLException {
+        return new ResponseEntity<List<Customers>>(customerService.getAllCustomers(), HttpStatus.OK);
     }
-    @DeleteMapping("customers/{customerID}")
-    public ResponseEntity<Void> deleteCustomer(int customerId) {
-        return null;
+
+    @GetMapping("/{customerId}")
+    public ResponseEntity<Customers> getCustomerById(@PathVariable int customerId) throws SQLException {
+        return new ResponseEntity<Customers>(customerService.getCustomerById(customerId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Integer> addCustomer(@RequestBody Customers customers) throws SQLException {
+        return new ResponseEntity<Integer>(customerService.addCustomer(customers), HttpStatus.OK);
+    }
+
+    @PutMapping("/{customerId}")
+    public ResponseEntity<Void> updateCustomer(@PathVariable int customerId, @RequestBody Customers customers) throws SQLException {
+        customerService.updateCustomer(customers);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{customerId}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable int customerId) throws SQLException {
+        customerService.deleteCustomer(customerId);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     public ResponseEntity<List<Transactions>> getAllTransactionsByCustomerId(int cutomerId) {
--- backend/src/main/java/com/wecp/progressive/service/CustomerServiceImplJpa.java
+++ backend/src/main/java/com/wecp/progressive/service/CustomerServiceImplJpa.java
@@ -1,5 +1,64 @@
 package com.wecp.progressive.service;
 
-public class CustomerServiceImplJpa {
+import java.sql.SQLException;
+import java.util.List;
+import java.util.*;
+
+import com.wecp.progressive.entity.Customers;
+
+public class CustomerServiceImplJpa implements CustomerService{
+
+    private List<Customers>  list = new ArrayList<Customers>();
+
+    @Override
+    public List<Customers> getAllCustomers() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Customers getCustomerById(int customerId) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public int addCustomer(Customers customers) throws SQLException {
+        return -1;
+    }
+
+    @Override
+    public void updateCustomer(Customers customers) throws SQLException {
+    }
+
+    @Override
+    public void deleteCustomer(int customerId) throws SQLException {
+    }
+
+    @Override
+    public List<Customers> getAllCustomersSortedByName() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public List<Customers> getAllCustomersFromArrayList() {
+        return list;
+    }
+
+    @Override
+    public List<Customers> addCustomersToArrayList(Customers customers) {
+        list.add(customers);
+        return list;
+    }
+
+    @Override
+    public List<Customers> getAllCustomersSortedByNameFromArrayList() {
+        List<Customers> sortedList = list;
+        Collections.sort(sortedList);
+        return sortedList;
+    }
+
+    @Override
+    public void emptyArrayList() {
+        list = new ArrayList<>();
+    }
     
 }
