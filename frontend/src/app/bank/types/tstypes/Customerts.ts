59e6441e6aad6992251ac58071e498b02da35157
username <wecp-dummy@gmail.com>
2024-02-17 05:15:34 +0000

    day 23 done

--- frontend/src/app/bank/types/tstypes/Customerts.ts
+++ frontend/src/app/bank/types/tstypes/Customerts.ts
@@ -1,30 +1,33 @@
-export class CustomerTS {
+export class Customer {
     customerId?: string;
     name: string;
     email: string;
-    password: string;
-    username: string;
-    role?: string;
-
-    constructor(name: string, email: string, username: string, password: string, role?: string, customerId?: string) {
-        this.customerId = customerId;
-        this.name = name;
-        this.email = email;
-        this.password = password;
-        this.username = username;
-        this.role = role;
-    }
-
-    displayInfo(){
-        console.log(`Customer ID: ${this.customerId}`);
-        console.log(`Name: ${this.name}`);
-        console.log(`Email: ${this.email}`);
-        console.log(`Username: ${this.username}`);
-        console.log(`Password: ${this.password}`);
-        console.log(`Role: ${this.role}`);
-        console.log("------");
-    }
-}
-
-const customer = new CustomerTS("John Doe", "john@example.com", "john_doe", "password123", "User");
-customer.displayInfo();
+    password:string;
+    username:string;
+    role?:string;
+  
+      constructor(data:any) {
+          this.customerId = data.customerId;
+          this.name = data.name;
+          this.email = data.email;
+          this.username = data.username;
+          this.password = data.password;
+          this.role = data.role;
+      }
+  
+      displayInfo() {
+          console.log(`Customer ID: ${this.customerId}`);
+          console.log(`Name: ${this.name}`);
+          console.log(`Email: ${this.email}`);
+          console.log(`Username: ${this.username}`);
+          console.log(`Password: ${this.password}`);
+          console.log(`Role: ${this.role}`);
+          console.log("------");
+      }
+  }
+  
+  
+  
+  
+  
+  
